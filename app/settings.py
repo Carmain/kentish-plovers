3928e9ee69fcce44b5ed6b606ae8162479220189
carmain <martin.l.billard@gmail.com>
2021-01-21 21:25:33 +0100

    Feature: templates inherit from base

--- app/settings.py
+++ app/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'website.apps.WebsiteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
-    'website',
 ]
 
 MIDDLEWARE = [
