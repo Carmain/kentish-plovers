97f85a0ab28e7ba831fd03d43055690b28e668be
carmain <martin.l.billard@gmail.com>
2021-01-24 22:41:40 +0100

    Feature: search form

--- website/urls.py
+++ website/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('search/<str:method>', views.search, name='search'),
+    path('search', views.search, name='search'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from website.utils.views_snippets import get_or_none
+from website.models import Plover
+from website.forms import CodeForm, MetalForm
 
 
 def index(request):
     return render(request, 'website/home.html')
+
+
+def search(request, method=None):
+    if method is None or (method != 'code' and method != 'metal'):
+        return redirect('search', method='code')
+
+    plover = None
+    form = None
+    if method == 'code':
+        plover = get_or_none(
+            Plover, code=request.POST.get('code'),
+            color=request.POST.get('color'))
+        form = CodeForm
+    else:
+        plover = get_or_none(
+            Plover, metal_ring=request.POST.get('metal_ring'))
+        form = MetalForm
+
+    data = {
+        'not_found': False,
+        'form_url': request.path
+    }
+
+    if request.method == 'POST':
+        form = form(request.POST)
+
+        if form.is_valid():
+            if plover:
+                data['plover'] = plover
+            else:
+                data['not_found'] = True
+    else:
+        form = form()
+
+    data['form'] = form
+    return render(request, 'website/search.html', data)
