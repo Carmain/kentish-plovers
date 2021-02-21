5460eaa9ee31fb4757d606488b6d6be4fd46d858
carmain <martin.l.billard@gmail.com>
2021-02-21 20:11:49 +0100

    Feature: add method to flush session storage by keys

--- website/utils/views_snippets.py
+++ website/utils/views_snippets.py
@@ -3,3 +3,9 @@ def get_or_none(class_model, **kwargs):
         return class_model.objects.get(**kwargs)
     except class_model.DoesNotExist:
         return None
+
+
+def flush_session(request, keys):
+    for key in keys:
+        if key in request.session:
+            del request.session[key]
