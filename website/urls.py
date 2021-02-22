80ee007698c2d2962cc6e2f8643c2816dc0ea4bb
carmain <martin.l.billard@gmail.com>
2021-02-22 23:21:30 +0100

    Feature: save plovers observations

--- website/urls.py
+++ website/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('search', views.search, name='search'),
     path('export-to-pdf/<metal_ring>', views.get_report, name='get_report'),
     path('map', views.map, name='map'),
+    path('observations', views.observations, name='observations'),
+    path('remove-plover/<str:id>', views.remove_plover_from_session,
+         name='remove_plover_from_session'),
+    path('validation', views.validate_plovers, name='validate_plovers'),
 ]
